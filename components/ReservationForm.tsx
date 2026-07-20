72140a92215ba6927d76062dc8703bd182a28d17
sezer117 <oncelsezer3@gmail.com>
2026-07-20 14:56:17 +0300

    Update ReservationForm.tsx
    Yarım saatlik rezervasyon saatleri

--- components/ReservationForm.tsx
+++ components/ReservationForm.tsx
@@ -3,7 +3,27 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 
-const SESSION_TIMES = ["09:00", "10:30", "12:00", "13:30", "15:00", "16:30"];
+const SESSION_TIMES = [
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+];
 
 function todayLocal() {
   const d = new Date();
